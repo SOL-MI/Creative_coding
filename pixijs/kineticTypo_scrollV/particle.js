db0490ca1b5fd18d39b5124f3cac8fc512e1b0a2
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-07-19 08:01:57 +0900

    refactor: downgrade particle movement

--- pixijs/kineticTypo_scrollV/particle.js
+++ pixijs/kineticTypo_scrollV/particle.js
@@ -1,12 +1,14 @@
 const FRICTION = 0.5;
 const MOVE_SPEED = 0.05;
 const RANDOM_FORCE = 5; // 무작위 힘
+const RANDOM_MULTIPLIER = (Math.random() - 0.5) * 2;
 
 export class Particle {
-  constructor(pos, texture) {
+  constructor(pos, texture, index) {
     this.sprite = new PIXI.Sprite(texture);
     this.sprite.scale.set(0.3);
-    this.sprite.tint = Math.random() * 0x2f2f2f;
+    this.sprite.tint = 0x2f2f2f;
+    // this.sprite.tint = Math.random() * 0x2f2f2f;
 
     this.savedX = pos.x;
     this.savedY = pos.y;
@@ -18,23 +20,27 @@ export class Particle {
     // this.vy = 0;
 
     // 초기 속도에 무작위 요소 추가
-    this.vx = (Math.random() - 0.5) * 2 * RANDOM_FORCE;
-    this.vy = (Math.random() - 0.5) * 2 * RANDOM_FORCE;
+    this.vx = RANDOM_MULTIPLIER * RANDOM_FORCE;
+    this.vy = RANDOM_MULTIPLIER * RANDOM_FORCE;
 
+    this.index = index;
     this.radius = 10;
     this.sprite.filters = [new PIXI.filters.BlurFilter()]; // 블러 필터 추가
   }
 
   draw() {
+    const dx = this.savedX - this.x;
+    const dy = this.savedY - this.y;
+
     // 자기 자리로 돌아가려는 힘 + 무작위 힘
-    this.vx +=
-      ((this.savedX - this.x) * MOVE_SPEED +
-        (Math.random() - 0.5) * RANDOM_FORCE) *
-      0.5;
-    this.vy +=
-      ((this.savedY - this.y) * MOVE_SPEED +
-        (Math.random() - 0.5) * RANDOM_FORCE) *
-      0.5;
+    this.vx += dx * MOVE_SPEED;
+    this.vy += dy * MOVE_SPEED;
+
+    // 홀수 번째 파티클만 랜덤 힘 추가
+    if (this.index % 2 === 1) {
+      this.vx += (Math.random() - 0.5) * RANDOM_FORCE * 0.5;
+      this.vy += (Math.random() - 0.5) * RANDOM_FORCE * 0.5;
+    }
 
     this.vx *= FRICTION;
     this.vy *= FRICTION;
